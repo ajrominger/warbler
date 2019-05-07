c27da037892b7e28e4f808b6ce0daa5e3ec16f2c
ajrominger <ajrominger@gmail.com>
2019-05-07 16:22:06 -0600

    got working for calculating all areas

--- get_range-overlap_area.R
+++ get_range-overlap_area.R
@@ -4,21 +4,60 @@ library(rgeos)
 
 load('data/wrange.RData')
 
+# reproject to equal area with km units
+wrange <- spTransform(wrange, CRS('+proj=cea +lon_0=-90 +lat_ts=0 +x_0=0 +y_0=0 +datum=WGS84 +units=km +no_defs'))
+
+# function to calculate area of overlap
 intersectionArea <- function(spgeom1, spgeom2) {
-    int <- rgeos::gIntersection(spgeom1, spgeom2)
+    # do the bounding boxes overlap
+    xlap <- bbox(spgeom1)[1, 1] <= bbox(spgeom2)[1, 2] & bbox(spgeom2)[1, 1] <= bbox(spgeom1)[1, 2]
+    ylap <- bbox(spgeom1)[2, 1] <= bbox(spgeom2)[2, 2] & bbox(spgeom2)[2, 1] <= bbox(spgeom1)[2, 2]
+    bboxOver <- xlap & ylap
     
-    if(is.null(int)) {
-        return(0)
+    a1 <- rgeos::gArea(spgeom1)
+    a2 <- rgeos::gArea(spgeom2)
+    
+    if(bboxOver) {
+        # if so calculate intersection
+        int <- rgeos::gIntersection(spgeom1, spgeom2)
+        
+        if(is.null(int)) {
+            return(c(area1 = a1, area2 = a2, intersection = 0))
+        } else {
+            # return the average of the overlaps from each spp perspective...or not, let
+            # that be caulculated with the output of the fun
+            # return(rgeos::gArea(int) / 2 * 
+            #            (1 / rgeos::gArea(spgeom1) + 1 / rgeos::gArea(spgeom2)))
+            
+            # return the actual overlap in addition to the two areas
+            return(c(area1 = a1, area2 = a2, intersection = rgeos::gArea(int)))
+        }
     } else {
-        return(sum(sapply(int@polygons, function(x) x@area)))
+        return(c(area1 = a1, area2 = a2, intersection = 0))
     }
 }
 
 
-intersectionArea(foo[2, ], foo[4, ])
+# make all sp-sp comparisons
+allComp <- outer(wrange$name, wrange$name, paste, sep = '_')
+allComp <- allComp[lower.tri(allComp)]
+allComp <- do.call(rbind, strsplit(allComp, '_'))
 
-foo <- wrange[wrange$name %in% c('Setophaga occidentalis', 'Setophaga townsendi', 'Setophaga pinus', 'Setophaga fusca'), ]
-foo
 
-bla <- gIntersection(foo[c(1, 2), ], foo[c(3, 4), ], byid = TRUE)
-bla@polygons[[2]]@area
+# loop through calculating overlap
+allROver <- parallel::mclapply(1:nrow(allComp), mc.cores = 10, FUN = function(i) {
+    print(i)
+    thisComp <- allComp[i, ]
+    intersectionArea(wrange[wrange$name == thisComp[1], ], wrange[wrange$name == thisComp[2], ])
+})
+allROver <- as.data.frame(do.call(rbind, allROver))
+
+# calculate various proportional overlaps and differences
+allROver$prop_interA1 <- allROver$itersection / allROver$area1
+allROver$prop_interA2 <- allROver$itersection / allROver$area2
+allROver$prop_interAvg <- (allROver$prop_interA1 + allROver$prop_interA2) / 2
+allROver$diff <- abs(allROver$area1 - allROver$area2)
+allROver$prop_diff <- allROver$diff / (allROver$area1 + allROver$area2 - allROver$intersection)
+
+# write out
+save(allROver, file = 'data/allROver.RData')
